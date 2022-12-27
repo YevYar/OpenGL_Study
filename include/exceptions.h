a533d32a8f2c598bdc0166e835060994454395a9
YevYar <kpyto2012@gmail.com>
2022-12-27 14:45:01 +0100

    Add GLRecAcquisitionException

---
+++ include/exceptions.h
@@ -0,0 +1,19 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <exception>
+
+namespace exceptions
+{
+	class GLRecAcquisitionException : public std::exception
+	{
+		public:
+			GLRecAcquisitionException(const char* const message) : std::exception(message)
+			{ }
+			GLRecAcquisitionException(const std::string& message) : std::exception(message.c_str())
+			{ }
+
+	};
+}
+
+#endif
