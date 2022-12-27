863c9c278988757ea97867f82096a03909693144
YevYar <kpyto2012@gmail.com>
2022-12-27 14:43:08 +0100

    Add macros to add/remove default constructors and operator= to class

---
+++ include/helpers/macros.h
@@ -0,0 +1,32 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define DEFAULT_COPYABLE(ClassX) \
+	ClassX(const ClassX&) = default;\
+	ClassX& operator=(const ClassX&) = default;
+
+#define DEFAULT_MOVABLE(ClassX) \
+	ClassX(ClassX&&) = default;\
+	ClassX& operator=(ClassX&&) = default;
+
+#define DEFAULT_COPYABLE_MOVABLE(ClassX) \
+	ClassX(const ClassX&) = default;\
+	ClassX& operator=(const ClassX&) = default;\
+	ClassX(ClassX&&) = default;\
+	ClassX& operator=(ClassX&&) = default;
+
+#define NOT_COPYABLE(ClassX) \
+	ClassX(const ClassX&) = delete;\
+	ClassX& operator=(const ClassX&) = delete;
+
+#define NOT_MOVABLE(ClassX) \
+	ClassX(ClassX&&) = delete;\
+	ClassX& operator=(ClassX&&) = delete;
+
+#define NOT_COPYABLE_MOVABLE(ClassX) \
+	ClassX(const ClassX&) = delete;\
+	ClassX& operator=(const ClassX&) = delete;\
+	ClassX(ClassX&&) = delete;\
+	ClassX& operator=(ClassX&&) = delete;
+
+#endif
