f5b8efd6ded161db8eefae890226c1dd809732e4
YevYar <kpyto2012@gmail.com>
2023-02-11 11:24:16 +0100

    Replace exception macros for classes by constructor inheritance

--- include/exceptions.h
+++ include/exceptions.h
@@ -5,55 +5,54 @@
 #include <string>
 
 /**
- * \brief EXCEPTION_DERIVED_FROM_CUSTOM_EXCEPTION is a macros to create derived from custom exception class with
- * two constructors to get exception message as std::string or as C-string.
+ * \brief exceptions namespace contains all custom exception types.
  */
-#define EXCEPTION_DERIVED_FROM_CUSTOM_EXCEPTION(Comment, Derived, Base) \
-    Comment\
-    class Derived : public Base\
-    {\
-    public:\
-        Derived(const char* const message) : Base(message)\
-        { }\
-        \
-        Derived(const std::string& message) : Base(message.c_str())\
-        { }\
-    \
-    };
-
-/**
- * \brief EXCEPTION_DERIVED_FROM_STD_EXCEPTION is a macros to create derived from std::exception class with
- * two constructors to get exception message as std::string or as C-string.
- */
-#define EXCEPTION_DERIVED_FROM_STD_EXCEPTION(Comment, X) \
-    EXCEPTION_DERIVED_FROM_CUSTOM_EXCEPTION(Comment, X, std::exception)
-
 namespace exceptions
 {
-	
-    EXCEPTION_DERIVED_FROM_STD_EXCEPTION(
-    /** 
-	 * \brief GLRecAcquisitionException is an exception to indicate error while generating some resources in OpenGL state machine.
-	 */,
-    GLRecAcquisitionException)
-
-    EXCEPTION_DERIVED_FROM_STD_EXCEPTION(
     /**
-	 * \brief FileException is a base exception to indicate error while working with files or directories.
-	 */,
-    FileException)
+     * \brief BaseException is a base class for exceptions which has all constructors of std::exception
+     * and defines additional constructor, which get error string as std::string.
+     */
+    class BaseException : public std::exception
+    {
+        using std::exception::exception;
+        
+        public:
+            BaseException(const std::string& message) : std::exception(message.c_str())
+            { }
+    };
 
-    EXCEPTION_DERIVED_FROM_CUSTOM_EXCEPTION(
     /**
-	 * \brief FileOpeningException is an exception to indicate error while opening file or directory.
-	 */,
-    FileOpeningException, FileException)
+     * \brief GLRecAcquisitionException is an exception to indicate error while generating some resources in OpenGL state machine.
+     */
+    class GLRecAcquisitionException : public BaseException
+    {
+        using BaseException::BaseException;
+    };
 
-    EXCEPTION_DERIVED_FROM_CUSTOM_EXCEPTION(
     /**
-	 * \brief FileReadingException is an exception to indicate error while reading file or directory.
-	 */,
-    FileReadingException, FileException)
+     * \brief FileException is a base exception to indicate error while working with files or directories.
+     */
+    class FileException : public BaseException
+    {
+        using BaseException::BaseException;
+    };
+
+    /**
+     * \brief FileOpeningException is an exception to indicate error while opening file or directory.
+     */
+    class FileOpeningException : public FileException
+    {
+        using FileException::FileException;
+    };
+
+    /**
+     * \brief FileReadingException is an exception to indicate error while reading file or directory.
+     */
+    class FileReadingException : public FileException
+    {
+        using FileException::FileException;
+    };
 }
 
 #endif
