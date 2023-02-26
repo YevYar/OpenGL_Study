d74a9b0f82180e549dde65217be5025380bbb865
YevYar <kpyto2012@gmail.com>
2023-02-26 20:44:27 +0100

    Change code to use OpenGL 4.6

--- resources/shaders/fs/fragmentShader.frag
+++ resources/shaders/fs/fragmentShader.frag
@@ -1,4 +1,4 @@
-#version 330 core
+#version 460 core
 
 in vec4 fColor;
 out vec4 FragColor;
--- src/window.cpp
+++ src/window.cpp
@@ -23,8 +23,8 @@ Window::Window(int width, int height, const std::string& title)
         cleanAndThrowOnInitException("Failed to initialize GLFW.");
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     auto window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
