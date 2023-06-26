1333b67baae4eef9a071dce4590a73d476614f62
YevYar <kpyto2012@gmail.com>
2023-06-26 21:44:47 +0200

    Implement functions to compare float numbers with precision

---
+++ include/helpers/floats.h
@@ -0,0 +1,53 @@
+#ifndef OGLS_HELPERS_FLOATS_H
+#define OGLS_HELPERS_FLOATS_H
+
+#include <cmath>
+
+namespace ogls::helpers
+{
+/**
+ * \brief The precision of float-type calculations.
+ */
+constexpr inline auto FLOAT_EPSILON = float{1.0E-8};
+
+/**
+ * \brief Returns the absolute value of x: |x|.
+ *
+ * Constexpr replacement of std::fabs().
+ *
+ * \param x - a value to get absolute value of.
+ * \return the absolute value of x.
+ */
+template<typename Type>
+constexpr Type absolute(Type x) noexcept
+{
+    return x >= Type{0} ? x : -x;
+}
+
+/**
+ * \brief Checks if provided float values a and b are equal with precision.
+ *
+ * \param precision - threshold value (an epsilon), which represents the acceptable difference
+ * between two floating-point numbers to consider them equal.
+ * \return true if a == b, false otherwise.
+ */
+constexpr bool isFloatsEqual(float a, float b, float precision = FLOAT_EPSILON) noexcept
+{
+    return absolute(a - b) < precision;
+}
+
+/**
+ * \brief Checks if provided float values a and b are not equal with precision.
+ *
+ * \param precision - threshold value (an epsilon), which represents the acceptable difference
+ * between two floating-point numbers to consider them equal.
+ * \return true if a != b, false otherwise.
+ */
+constexpr bool isFloatsNotEqual(float a, float b, float precision = FLOAT_EPSILON) noexcept
+{
+    return !isFloatsEqual(a, b, precision);
+}
+
+}  // namespace ogls::helpers
+
+#endif
