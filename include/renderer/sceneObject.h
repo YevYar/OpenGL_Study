bb81e3f55fa19b0ba411e1422d4740a5a385314c
YevYar <kpyto2012@gmail.com>
2024-05-05 17:16:15 +0300

    Make SceneObject use renderer::Material and renderer::Mesh

--- include/renderer/sceneObject.h
+++ include/renderer/sceneObject.h
@@ -1,13 +1,9 @@
 #ifndef OGLS_RENDERER_SCENE_OBJECT_H
 #define OGLS_RENDERER_SCENE_OBJECT_H
 
-#include <memory>
-#include <vector>
-
-#include "helpers/macros.h"
-#include "openglCore/shaderProgram.h"
-#include "openglCore/textureUnit.h"
-#include "openglCore/vertexArray.h"
+#include "mathCore/transformMatrix.h"
+#include "renderer/material.h"
+#include "renderer/mesh.h"
 
 namespace ogls::renderer
 {
@@ -17,25 +13,145 @@ namespace ogls::renderer
 class SceneObject
 {
     public:
+        /**
+         * \brief Size represents the size in pixels of 3 dimensions of the SceneObject.
+         */
+        struct Size final
+        {
+            public:
+                /**
+                 * \brief The size in pixels of the dimension along the X axis.
+                 */
+                size_t length = {0};
+                /**
+                 * \brief The size in pixels of the dimension along the Y axis.
+                 */
+                size_t height = {0};
+                /**
+                 * \brief The size in pixels of the dimension along the Z axis.
+                 */
+                size_t width  = {0};
+
+        };  // struct Size
+
+    public:
         SceneObject() = default;
         OGLS_DEFAULT_COPYABLE_MOVABLE(SceneObject)
         /**
-         * \brief Constructs new SceneObject using a vertex array object and a shader program.
+         * \brief Constructs new SceneObject using Mesh and Material.
          *
-         * \param vao           - a vertex array object, which contains necessary vertex configuration.
-         * \param shaderProgram - a shader program, which is used for rendering.
+         * \param mesh           - a model (Mesh) of the object. It's passed as shared_ptr, because it's expected,
+         * that one Mesh can be used to render several SceneObject -s using different Material -s.
+         * \param material       - a visual property (Material) of the object.
+         * \param size           - the size of the SceneObject in pixels.
+         * \param globalPosition - the Point in global space, in which the SceneObject will be spawned.
          */
-        SceneObject(std::shared_ptr<ogls::oglCore::vertex::VertexArray>   vao,
-                    std::shared_ptr<ogls::oglCore::shader::ShaderProgram> shaderProgram);
+        SceneObject(std::shared_ptr<Mesh> mesh, Material material, Size size,
+                    mathCore::Point<int> globalPosition = mathCore::Point<int>{0, 0, 0}) noexcept;
         virtual ~SceneObject() noexcept = default;
 
-        /**
-         * \brief Sets a configuration of texture units, which is used for rendering of the object.
-         *
-         * \param texturesConfiguration - a configuration of texture units.
-         */
-        virtual void setTexturesConfiguration(ogls::oglCore::texture::TexturesConfiguration texturesConfiguration);
+        //------ ACCESSORS TO DIMENSIONAL PROPERTIES
 
+        /**
+         * \brief Returns the height of the object in pixels.
+         */
+        int  getHeight() const noexcept;
+        /**
+         * \brief Returns the length of the object in pixels.
+         */
+        int  getLength() const noexcept;
+        /**
+         * \brief Returns the size of the object.
+         */
+        Size getSize() const noexcept;
+        /**
+         * \brief Returns the width of the object in pixels.
+         */
+        int  getWidth() const noexcept;
+        /**
+         * \brief Sets the height of the object in pixels.
+         *
+         * \param height - the height in pixels to set.
+         */
+        void setHeight(int height) noexcept;
+        /**
+         * \brief Sets the length of the object in pixels.
+         *
+         * \param length - the length in pixels to set.
+         */
+        void setLength(int length) noexcept;
+        /**
+         * \brief Sets the size of the object.
+         *
+         * \param size - the size to set.
+         */
+        void setSize(Size size) noexcept;
+        /**
+         * \brief Sets the size of the object.
+         *
+         * \param length - the length in pixels to set.
+         * \param height - the height in pixels to set.
+         * \param width  - the width in pixels to set.
+         */
+        void setSize(int length, int height, int width) noexcept;
+        /**
+         * \brief Sets the width of the object in pixels.
+         *
+         * \param width - the width in pixels to set.
+         */
+        void setWidth(int width) noexcept;
+
+        //------ ACCESSORS TO POSITION
+
+        /**
+         * \brief Returns the position of the SceneObject in global coordinates.
+         */
+        mathCore::Point<int> getPositionInGlobal() const noexcept;
+        /**
+         * \brief Returns the position of the SceneObject relative to its parent.
+         */
+        mathCore::Point<int> getPositionInParent() const noexcept;
+        /**
+         * \brief Sets the position of the SceneObject in global coordinates.
+         *
+         * \param position - the new position of the SceneObject in global coordinates.
+         */
+        void                 setPositionInGlobal(mathCore::Point<int> position);
+        /**
+         * \brief Sets the position of the SceneObject relative to its parent.
+         *
+         * \param position - the new position of the SceneObject relative to its parent.
+         */
+        void                 setPositionInParent(mathCore::Point<int> position);
+
+        //------ ACCESSORS TO VISUAL PROPERTIES
+
+        /**
+         * \brief Returns a reference to the Material associated with this object.
+         */
+        Material&             getMaterial() noexcept;
+        /**
+         * \brief Returns a constant reference to the Material associated with this object.
+         */
+        const Material&       getMaterial() const noexcept;
+        /**
+         * \brief Returns the Mesh associated with this object.
+         */
+        std::shared_ptr<Mesh> getMesh() const noexcept;
+        /**
+         * \brief Sets the Material associated with this object.
+         *
+         * \param material - the new Material to set.
+         */
+        void                  setMaterial(Material material) noexcept;
+        /**
+         * \brief Sets the Mesh associated with this object.
+         *
+         * \param mesh - the new Mesh to set.
+         */
+        void                  setMesh(std::shared_ptr<Mesh> mesh) noexcept;
+
+    private:
         /**
          * \brief Renders something on the scene.
          */
@@ -43,25 +159,26 @@ class SceneObject
 
     protected:
         /**
-         * \brief Applies set textures configuration to be used while the following rendering.
+         * \brief The unique identifier of the SceneObject.
          *
-         * See ogls::oglCore::texture::applyTexturesConfiguration().
+         * 0 identifies an invalid empty SceneObject.
          */
-        void applyTexturesConfiguration();
+        size_t                    m_id = {0};
+        /**
+         * \brief The Material used to render the SceneObject.
+         */
+        Material                  m_material;
+        /**
+         * \brief The Mesh representing the geometry of the SceneObject.
+         */
+        std::shared_ptr<Mesh>     m_mesh = nullptr;
+        /**
+         * \brief The model matrix of the object.
+         */
+        mathCore::TransformMatrix m_modelMatrix;
 
-    protected:
-        /**
-         * \brief Shader program, which is used to render this object.
-         */
-        std::shared_ptr<ogls::oglCore::shader::ShaderProgram> m_shaderProgram = nullptr;
-        /**
-         * \brief Configuration of texture units, which is used to render this object.
-         */
-        ogls::oglCore::texture::TexturesConfiguration         m_texturesConfiguration;
-        /**
-         * \brief Vertex array object, which is used to render this object.
-         */
-        std::shared_ptr<ogls::oglCore::vertex::VertexArray>   m_vao = nullptr;
+
+        friend class Renderer;
 
 };  // SceneObject
 
--- src/renderer/sceneObject.cpp
+++ src/renderer/sceneObject.cpp
@@ -2,21 +2,100 @@
 
 namespace ogls::renderer
 {
-SceneObject::SceneObject(std::shared_ptr<ogls::oglCore::vertex::VertexArray>   vao,
-                         std::shared_ptr<ogls::oglCore::shader::ShaderProgram> shaderProgram) :
-    m_vao{std::move(vao)},
-    m_shaderProgram{std::move(shaderProgram)}
+SceneObject::SceneObject(std::shared_ptr<Mesh> mesh, Material material, Size size,
+                         mathCore::Point<int> globalPosition) noexcept :
+    m_material{std::move(material)},
+    m_mesh{std::move(mesh)}
 {
 }
 
-void SceneObject::setTexturesConfiguration(ogls::oglCore::texture::TexturesConfiguration texturesConfiguration)
+//------ ACCESSORS TO DIMENSIONAL PROPERTIES
+
+int SceneObject::getHeight() const noexcept
 {
-    m_texturesConfiguration = std::move(texturesConfiguration);
+    return 0;
 }
 
-void SceneObject::applyTexturesConfiguration()
+int SceneObject::getLength() const noexcept
 {
-    ogls::oglCore::texture::applyTexturesConfiguration(m_texturesConfiguration);
+    return 0;
+}
+
+SceneObject::Size SceneObject::getSize() const noexcept
+{
+    return Size{};
+}
+
+int SceneObject::getWidth() const noexcept
+{
+    return 0;
+}
+
+void SceneObject::setHeight(int height) noexcept
+{
+}
+
+void SceneObject::setLength(int length) noexcept
+{
+}
+
+void SceneObject::setSize(Size size) noexcept
+{
+}
+
+void SceneObject::setSize(int length, int height, int width) noexcept
+{
+}
+
+void SceneObject::setWidth(int width) noexcept
+{
+}
+
+//------ ACCESSORS TO POSITION
+
+mathCore::Point<int> SceneObject::getPositionInGlobal() const noexcept
+{
+    return mathCore::Point<int>{};
+}
+
+mathCore::Point<int> SceneObject::getPositionInParent() const noexcept
+{
+    return mathCore::Point<int>{};
+}
+
+void SceneObject::setPositionInGlobal(mathCore::Point<int> position)
+{
+}
+
+void SceneObject::setPositionInParent(mathCore::Point<int> position)
+{
+}
+
+//------ ACCESSORS TO VISUAL PROPERTIES
+
+Material& SceneObject::getMaterial() noexcept
+{
+    return m_material;
+}
+
+const Material& SceneObject::getMaterial() const noexcept
+{
+    return m_material;
+}
+
+std::shared_ptr<Mesh> SceneObject::getMesh() const noexcept
+{
+    return m_mesh;
+}
+
+void SceneObject::setMaterial(Material material) noexcept
+{
+    m_material = std::move(material);
+}
+
+void SceneObject::setMesh(std::shared_ptr<Mesh> mesh) noexcept
+{
+    m_mesh = mesh;
 }
 
 }  // namespace ogls::renderer
